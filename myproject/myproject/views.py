5cd87a3cd0b5d0df99ce0ac6a3c985fa6a2750bb
Mayank Singh <mayanksingh0409@gmail.com>
2026-06-30 23:24:34 +0530

    Delete unused view functions
    Removed unused view functions from views.py.

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,17 +1,1 @@
-from django.shortcuts import render
 
-def home(request):
-    return render(request, 'index.html')
-
-def main(request):
-    return render(request, 'main.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def login_(request):
-    return render(request,  'login.html')
-
-def logout(request):
-    return render(request,  'logout.html')
